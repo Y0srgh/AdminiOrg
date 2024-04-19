4e619c2d76efd37e4fe8dd3342ca802053a6b269
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-04-19 17:44:59 +0100

    link to db

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,6 @@
 export const PORT = 5000;
 
-export const mongoDBURL = 'mongodb://localhost:27017/Admin-Org'
+export const mongoDBURL = 
+//'mongodb://localhost:27017/Admin-Org'
+'mongodb+srv://yosrghozzi:kUz8FdVc72qBUym8@cluster0.ctkwhfj.mongodb.net/AdminiOrga'
 //kUz8FdVc72qBUym8
