2d6b0f299f39399dca83ab46a2c1fa9cf01a7d93
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-04-26 22:29:57 +0100

    fix bug

--- backend/routes/attestationRoutes.js
+++ backend/routes/attestationRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { attestationRequest } from "../controllers/attestationController";
+import { attestationRequest } from "../controllers/attestationController.js";
 const router = express.Router();
 router.post("/", attestationRequest);
 export default router;
